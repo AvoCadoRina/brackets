f79e87aa7f86b47b38c90ea6f118ea81f7577c6b
Ekaterina <78584614+AvoCadoRina@users.noreply.github.com>
2021-03-12 23:33:46 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+     unev = str.length % 2;
+  if (unev != 0) 
+      return false;
+verticalnae = 0
+krug_ot = 0
+krug_zak = 0
+kvad_ot = 0
+kvad_zak = 0
+    for(var i = 0; i < str.length; i++){
+    if (str[i] == '|'){
+        verticalnae =+1;
+    } else if (str[i] == '(') {
+    krug_ot =+1;
+          } else if (str[i] == ')') {
+    krug_zak =+1;
+            } else if (str[i] == '[') {
+    kvad_ot =+1;
+                           } else if (str[i] == ']') {
+    kvad_zak =+1;
+                    } else if (str[i] == '(') {
+    krug_ot =+1;
+                               }
+                               
+    if (verticalnae%2 === 0)|krug_ot = krug_zak |kvad_ot=kvad_zak){
+    return true;
+}
+    else
+        return false;
 }
