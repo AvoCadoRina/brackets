6a272f38ae6d3c98e43a6a431dbd7c89672ed410
Ekaterina <78584614+AvoCadoRina@users.noreply.github.com>
2021-03-13 00:17:42 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-     pervichka = str.length % 2;
-  if (pervichka != 0) 
-      return false;
+if (str.length%2 != 0) return false;
 verticalnae = 0
 krug_ot = 0
 krug_zak = 0
 kvad_ot = 0
 kvad_zak = 0
-    for(var i = 0; i < str.length; i++){
+    for(str i = 0; i < str.length; i++){
     if (str[i] == '|'){
         verticalnae =+1;
     } else if (str[i] == '(') {
@@ -18,9 +16,7 @@ kvad_zak = 0
     kvad_ot =+1;
                            } else if (str[i] == ']') {
     kvad_zak =+1;
-                    } else if (str[i] == '(') {
-    krug_ot =+1;
-                               }
+                    }
                                
     if (verticalnae%2 === 0 && krug_ot === krug_zak && kvad_ot===kvad_zak){
     return true;
